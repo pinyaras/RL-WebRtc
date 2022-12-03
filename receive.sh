7f74b047c58f5ecc319830ca9df835c6e031c32e
Ezra Brooks <ezbrooks@users.noreply.github.com>
2022-12-03 10:33:02 -0500

    Remove outdated files

--- receive.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-# AlphaRTC Docker receive command (modify config file as needed)
-docker run -d --network=host --rm -v `pwd`:/app -w /app --name alphartc_pyinfer opennetlab.azurecr.io/challenge-env peerconnection_serverless receiver_pyinfer.json
--- send.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-# AlphaRTC Docker send command (modify config file as needed)
-docker exec alphartc_pyinfer peerconnection_serverless sender_pyinfer.json
